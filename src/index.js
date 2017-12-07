f399d35a47f9d535164ca842f11cf3db4134c27b
Aleksandr Maksimovic <aleksandr.maksimoviclt@gmail.com>
2017-12-07 16:15:25 +0100

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
 import http from 'http';
 import express from 'express';
+import mongoose from 'mongoose';
+
 import morgan from 'morgan';
-import config from './config.json';
+import config from './server/config/config';
+
 
 let app = express();
+
 app.server = http.createServer(app);
 
-// logger
-app.use(morgan('dev'));
+mongoose.connect(config.db, {
+  useMongoClient: true
+});
+
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${config.db}`);
+});
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to database: ${config.db}`);
+});
+
+if (config.env === 'development') {
+  mongoose.set('debug', true);
+}
 
 app.server.listen(process.env.PORT || config.port, () => {
+  console.log(`API Server started and listening on port ${config.port} (${config.env})`);
 });
+
+app.use(morgan('dev'));
 
 
 export default app;
