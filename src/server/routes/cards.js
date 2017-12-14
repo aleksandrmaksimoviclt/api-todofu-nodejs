24bf663ebb987b790c34106a03e0f4bd221d4c57
Aleksandr Maksimovic <aleksandr.maksimoviclt@gmail.com>
2017-12-14 18:15:42 +0100

    Add Card routes

---
+++ src/server/routes/cards.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cardCtrl from '../controllers/cards';
+
+// import auth from '../config/jwt';
+
+const router = express.Router();
+
+router.route('/')
+  /** GET /api/card - Get list of cards */
+  .get(cardCtrl.list)
+  // .get(auth, userCtrl.list)
+
+  /** POST /api/card - Create new card */
+  .post(cardCtrl.create);
+
+router.route('/:cardId')
+  /** GET /api/list/:listId - Get list */
+  .get(cardCtrl.get)
+
+  /** PUT /api/list/:listId - Update list */
+  .put(cardCtrl.update)
+
+  /** DELETE /api/list/:listId - Delete user */
+  .delete(cardCtrl.remove);
+
+// /** Load user when API with userId route parameter is hit */
+// router.param('userId', cardCtrl.load);
+
+export default router;
