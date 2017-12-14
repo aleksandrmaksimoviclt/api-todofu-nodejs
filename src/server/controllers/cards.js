e1f64203a169782b6dc9e497a0bc27f9e7fe2f7b
Aleksandr Maksimovic <aleksandr.maksimoviclt@gmail.com>
2017-12-14 18:13:33 +0100

    Add Cards controller

---
+++ src/server/controllers/cards.js
@@ -0,0 +1,52 @@
+import Card from '../models/card';
+
+function load(req, res, next, id) {
+  Card.findById(id)
+    .exec()
+    .then((card) => {
+      req.dbCard = card;
+      return next();
+    }, (e) => next(e));
+}
+
+function get(req, res) {
+  return res.json(req.dbCard);
+}
+
+function create(req, res, next) {
+  Card.create({
+      title: req.body.title,
+      list: req.body.list
+    })
+    .then((savedCard) => {
+      return res.json(savedCard);
+    }, (e) => next(e));
+}
+
+function update(req, res, next) {
+  const card = req.dbCard;
+  Object.assign(card, req.body);
+
+  card.save()
+    .then((savedCard) => res.sendStatus(204),
+      (e) => next(e));
+}
+
+function list(req, res, next) {
+  const { limit = 50, skip = 0 } = req.query;
+  Card.find()
+    .skip(skip)
+    .limit(limit)
+    .exec()
+    .then((cards) => res.json(cards),
+      (e) => next(e));
+}
+
+function remove(req, res, next) {
+  const card = req.dbCard;
+  card.remove()
+    .then(() => res.sendStatus(204),
+      (e) => next(e));
+}
+
+export default { load, get, create, update, list, remove };
