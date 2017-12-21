02d7c487895dabe540268e14df703d9c3d7bb551
Aleksandr Maksimovic <aleksandr.maksimoviclt@gmail.com>
2017-12-21 18:18:26 +0100

    Save reference of listID on card save

--- src/server/models/card.js
+++ src/server/models/card.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
+import List from './list';
+
 export const CardSchema = new mongoose.Schema({
   list: {
     type: mongoose.Schema.Types.ObjectId,
@@ -9,6 +11,24 @@ export const CardSchema = new mongoose.Schema({
     type: String,
     required: true,
     trim: true
+  }
+});
+
+CardSchema.post('save', function(card, next) {
+  if(card.list){
+    List
+      .findById(card.list)
+      .exec()
+      .then((list) => {
+        mongoose.model('Card', CardSchema)
+          .find({list: list._id})
+          .exec()
+          .then((cards) => {
+            list.cards = cards;
+            list.save()
+            next()
+          });
+      });
   }
 });
 
